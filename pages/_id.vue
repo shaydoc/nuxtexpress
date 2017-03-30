935e1bf0366d934d9882a971fed42e5e558d472e
seamus doherty <shaydoc@seamuss-MacBook-Pro.local>
2017-03-30 22:11:21 +0100

    remove image

--- pages/_id.vue
+++ pages/_id.vue
@@ -1,7 +1,6 @@
 <template>
   <section class="container">
-    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
-    <h1 class="title">
+     <h1 class="title">
       User
     </h1>
     <h2 class="info">
