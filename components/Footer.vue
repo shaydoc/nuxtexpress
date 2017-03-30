4c3f16abb0f65509f8ec607d497ee7043dbdffa0
seamus doherty <shaydoc@seamuss-MacBook-Pro.local>
2017-03-30 22:15:51 +0100

    remove logo

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,11 +3,7 @@
         <div class="container">
             <div class="row">
                 <ul>
-                    <li><a href="http://www.nidirect.gov.uk/crown-copyright" target="_blank" class="elink" title="link opens in a new browser window">©  Crown copyright</a></li>
-                    <li><a href="http://www.nidirect.gov.uk/terms-and-conditions" target="_blank" class="elink" title="link opens in a new browser window">Terms and conditions</a></li>
-                    <li><a href="http://www.nidirect.gov.uk/privacy" target="_blank" class="elink" title="link opens in a new browser window">Privacy</a></li>
-                    <li><a href="http://www.nidirect.gov.uk/cookies" target="_blank" class="elink" title="link opens in a new browser window">Cookies</a></li>
-                    <li><a href="resources.html">Resources</a></li>
+                  
                 </ul>
             </div>
         </div>
